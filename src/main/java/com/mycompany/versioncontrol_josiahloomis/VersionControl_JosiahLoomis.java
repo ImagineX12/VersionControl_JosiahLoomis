11ef4fa7ce74cddd1a7ca46956856b4bba5694a6
ImagineX12 <86370767+ImagineX12@users.noreply.github.com>
2025-09-25 13:36:13 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/versioncontrol_josiahloomis/VersionControl_JosiahLoomis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.versioncontrol_josiahloomis;
+
+/**
+ *
+ * @author Imagine
+ */
+public class VersionControl_JosiahLoomis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
